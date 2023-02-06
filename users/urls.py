d8581f64fe6c66494bc11768a495d59d29fa7248
notkingsley <103395094+notkingsley@users.noreply.github.com>
2023-02-06 20:56:08 +0100

    redesigned base template. added login/logout

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
+	path("login", views.LoginView.as_view(), name= "login"),
+	path("logout", views.LogoutView.as_view(), name= "logout"),
 	path("u/<str:username>/", views.ProfileView.as_view(), name= "profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import models
+
+# TODO accept login by email
+
+HOMEPAGE = reverse_lazy("twitterx:home")
 
 
 class ProfileView(generic.DetailView):
@@ -8,3 +14,12 @@ class ProfileView(generic.DetailView):
 	model = models.User
 	slug_field: str = "username"
 	slug_url_kwarg: str = "username"
+
+
+class LoginView(auth_views.LoginView):
+	template_name: str = "users/login.html"
+	next_page = HOMEPAGE
+
+
+class LogoutView(auth_views.LogoutView):
+	next_page = HOMEPAGE
