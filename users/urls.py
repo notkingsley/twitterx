9ec75d7770ac0712c9c328a6a9acc63af9bb6e3f
notkingsley <103395094+notkingsley@users.noreply.github.com>
2023-02-07 20:41:13 +0100

    added password resetting

--- users/urls.py
+++ users/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
 
 	path("password/change/", views.PasswordChangeView.as_view(), name= "password_change"),
 	path("password/change/done/", views.PasswordChangeDoneView.as_view(), name= "password_change_done"),
+	path("password/reset/", views.PasswordResetView.as_view(), name= "password_reset"),
+	path("password/reset/done/", views.PasswordResetDoneView.as_view(), name= "password_reset_done"),
+	path("password/reset/confirm/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(), name= "password_reset_confirm"),
+	path("password/reset/complete/", views.PasswordResetCompleteView.as_view(), name= "password_reset_complete"),
 	
 	path("u/<str:username>/", views.ProfileView.as_view(), name= "profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -79,3 +79,23 @@ class PasswordChangeView(auth_views.PasswordChangeView):
 
 class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
 	template_name: str = "users/password_change_done.html"
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+	template_name: str = "users/password_reset.html"
+	email_template_name: str = "users/password_reset_email.html"
+	subject_template_name: str = "users/password_reset_subject.txt"
+	success_url = reverse_lazy("users:password_reset_done")
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+	template_name: str = "users/password_reset_done.html"
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+	template_name: str = "users/password_reset_confirm.html"
+	success_url = reverse_lazy("users:password_reset_complete")
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+	template_name: str = "users/password_reset_complete.html"
