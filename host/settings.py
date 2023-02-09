bb4290fd1288eb2b602cb99c1b92c67e92357214
notkingsley <103395094+notkingsley@users.noreply.github.com>
2023-02-09 22:03:59 +0100

    new app profiles

--- host/settings.py
+++ host/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "profiles.apps.ProfilesConfig",
     "twitterx.apps.TwitterxConfig",
     "users.apps.UsersConfig",
 ]
--- host/urls.py
+++ host/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", include("profiles.urls")),
+    path("", include("twitterx.urls")),
     path("", include("users.urls")),
     path('admin/', admin.site.urls),
-    path("", include("twitterx.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
