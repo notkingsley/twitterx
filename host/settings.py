0a12dcb09f9333e71ee9405859662063c401a581
notkingsley <103395094+notkingsley@users.noreply.github.com>
2023-02-13 23:18:31 +0100

    feeds app to handle tweet streaming

--- host/settings.py
+++ host/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "feeds.apps.FeedsConfig",
     "profiles.apps.ProfilesConfig",
     "twitterx.apps.TwitterxConfig",
     "tweets.apps.TweetsConfig",
