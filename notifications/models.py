43197762b6de15b99f81771b4070547acf13e756
notkingsley <103395094+notkingsley@users.noreply.github.com>
2023-02-28 22:20:33 +0100

    add notifications to site

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class NotificationBox(models.Model):
+	user = models.OneToOneField(
+		"users.User",
+		models.CASCADE,
+		related_name= "notification_box",
+		editable= False,
+	)
+
+
+class Message(models.Model):
+	body = models.CharField(
+		"Notification message",
+		max_length= 280,
+	)
+
+	link = models.URLField(
+		"Redirect link",
+	)
+
+	created = models.DateTimeField(
+		"Date created",
+		auto_now_add= timezone.now
+	)
+
+	box = models.ForeignKey(
+		NotificationBox,
+		models.CASCADE,
+		related_name= "messages",
+		verbose_name= "Notification Box",
+	)
+
+	clicked = models.BooleanField(
+		"Notification has been clicked",
+	)
+
+	class Meta:
+		ordering = ["-created"]
+	
+
+	def __str__(self) -> str:
+		return self.body
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth import mixins
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import models
+
+
+LOGIN_URL = reverse_lazy("users:login")
+
+REDIRECT_FIELD_NAME = 'r'
+
+
+def _ensure_box(user):
+	"""
+	Ensure user has a notification box
+	"""
+	try:
+		user.notification_box
+	except:
+		models.NotificationBox.objects.create(user= user)
+
+
+class Notifications(mixins.LoginRequiredMixin, generic.ListView):
+	login_url = LOGIN_URL
+	redirect_field_name = REDIRECT_FIELD_NAME
+	context_object_name = "messages"
+	template_name = "notifications/notifications.html"
+
+	def get_queryset(self):
+		_ensure_box(self.request.user)
+		return self.request.user.notification_box.messages.all()
